10b7ee92607947ddbd0381a683c1ae9304e0e43a
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-24 19:15:08 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,30 @@ class Song
   def save
     self.class.all << self
   end
+      def self.create
+     song = self.new
+     song.save
+     song
+    end
+
+    def self.new_by_name(new_song)
+     song = self.new
+     song.name = new_song
+     song
+    end
+
+    def Song.create_by_name(new_song)
+     song=self.new
+     song.name = new_song
+     @@all << song
+     song
+    end
+
+    def Song.find_by_name(new_song)
+     song=self.new
+     song.name = new_song
+
+     #@@all.each do  |s|  s.name =
+    end
 
 end
