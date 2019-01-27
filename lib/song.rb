cd0079d5343f0cd2c2c9fe8d83e91ce57f686212
Robert McCary <robert.mccary@yahoo.com>
2019-01-27 19:27:43 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,8 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  
+
   def self.all
     @@all
   end
@@ -10,4 +12,58 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.name = name
+    @@all << song
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(mp3_formatted_file)
+    
+    data = self.new 
+    data.name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[1] 
+    data.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    data
+  end
+  
+  def self.create_from_filename(mp3_formatted_file)
+    data = self.new 
+    data.name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[1] 
+    data.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    data.save
+    data
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
