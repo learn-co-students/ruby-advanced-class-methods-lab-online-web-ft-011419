14ef8140e9ab1cbf59e47f8f8cf9882db049df02
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-30 12:00:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,7 @@ class Song
     new_song.name = song_name 
     return new_song
   end
-  def self.find_by_name()
-    song.all.find
+  def self.find_by_name(name)
+    song.all.find {song.name == name}
   end
 end
