e8b5ab461f9a452d4b933dc67b3e3e070b53b132
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-30 11:07:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,9 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  def initialize
+    @@all << self
+  end
   def self.all
     @@all
   end
@@ -9,5 +11,8 @@ class Song
   def save
     self.class.all << self
   end
-
+  def self.new_by_name(name)
+    new_song=self.new
+    new_song =  
+  end
 end
