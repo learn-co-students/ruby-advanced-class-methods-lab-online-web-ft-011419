652467c26e54c4ea62789293850ea1a41cff50bd
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-30 11:41:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,12 +7,15 @@ class Song
   def self.all
     @@all
   end
-
+def self.create
+  self.new
+end
   def save
     self.class.all << self
   end
-  def self.new_by_name(name)
+  def self.create_by_name(name)
     new_song=self.new
-    new_song. 
+    new_song.name = name 
   end
+  def self.find_by_name
 end
